f5b396e8d1b4b1e7651f21a19b317b753be8d470
e10101 <e10101@users.noreply.github.com>
2024-09-15 19:46:30 +0800

    Run Axum

--- axum-project/src/main.rs
+++ axum-project/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/", 
+        get(|| async { Html("Hello <strong>world</strong>")}));
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("->> LISTENING on {:?}\n", listener.local_addr());
+
+    axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
