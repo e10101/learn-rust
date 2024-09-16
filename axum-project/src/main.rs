b8b2cb9593e844d2a8e16a8d0a2ce1126c5deb28
e10101 <e10101@users.noreply.github.com>
2024-09-16 08:26:22 +0800

    Add path params

--- axum-project/src/main.rs
+++ axum-project/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use axum::{extract::Query, handler, http, response::{Html, IntoResponse}, routing::get, Router};
+use axum::{extract::{Path, Query}, handler, http, response::{Html, IntoResponse}, routing::get, Router};
 use serde::Deserialize;
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
@@ -22,6 +22,7 @@ async fn main() {
 
     let routes_hello = Router::new()
         .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
         .layer(trace_layer);
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -41,4 +42,9 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
     let name = params.name.as_deref().unwrap_or("world");
     Html(format!("<h1>Hello <strong>{name}</strong></h1>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
--- axum-project/tests/quick_dev.rs
+++ axum-project/tests/quick_dev.rs
@@ -7,10 +7,8 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
 
     println!("Attempting to connect to http://127.0.0.1:8080");
-    let response = hc.do_get("/hello?name=John").await?;
-
-    println!("Response status: {}", response.status());
-    response.print().await?;
+    hc.do_get("/hello?name=John").await?.print().await?;
+    hc.do_get("/hello2/Mike").await?.print().await?;
 
     Ok(())
 }
